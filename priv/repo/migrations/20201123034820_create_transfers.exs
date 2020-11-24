42f183bddeb11c81aee81fd0eb444f17b45962fb
Leandro Neves <leandrosoares6@hotmail.com>
2020-11-23 23:26:13 -0300

    Migration create_transfers added

--- priv/repo/migrations/20201123034820_create_transfers.exs
+++ priv/repo/migrations/20201123034820_create_transfers.exs
@@ -4,12 +4,10 @@ defmodule GraphBanking.Repo.Migrations.CreateTransfers do
   def change do
     create table(:transfers, primary_key: false) do
       add :uuid, :binary_id, primary_key: true
+      add :sender, references(:accounts, column: :uuid, type: :binary_id)
       add :address, :binary_id
       add :amount, :float
       add :when, :utc_datetime_usec
-      add :account_id, references(:accounts, column: :uuid, type: :binary_id)
-
-      timestamps()
     end
   end
 end
