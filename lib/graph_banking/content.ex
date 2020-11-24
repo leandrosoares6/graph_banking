fc5fbc2f8f6d09b879533cec6283491ddfa9746a
Leandro Neves <leandrosoares6@hotmail.com>
2020-11-23 23:23:32 -0300

    transfer_money function added in Content

--- lib/graph_banking/content.ex
+++ lib/graph_banking/content.ex
@@ -1,6 +1,7 @@
 defmodule GraphBanking.Content do
   alias GraphBanking.Repo
   alias GraphBanking.Schema.Account
+  alias GraphBanking.Schema.Transfer
 
   def list_accounts() do
     Repo.all(Account)
@@ -9,6 +10,12 @@ defmodule GraphBanking.Content do
   def create_account(attrs, _info) do
     %Account{}
     |> Account.changeset(attrs)
-    |> Repo.insert
+    |> Repo.insert()
+  end
+  
+  def transfer_money(attrs, _info) do
+    %Transfer{}
+    |> Transfer.changeset(attrs)
+    |> Repo.insert()
   end
 end
