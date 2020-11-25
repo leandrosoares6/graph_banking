716234a852ce3251baab9b24c8296a20d9fc0998
Leandro Neves <leandrosoares6@hotmail.com>
2020-11-25 14:38:32 -0300

    Custom changeset validation to avoid a transfer to yourself

--- lib/graph_banking/schema/transfer.ex
+++ lib/graph_banking/schema/transfer.ex
@@ -1,7 +1,5 @@
 defmodule GraphBanking.Schema.Transfer do
-  # use Ecto.Schema
   use GraphBanking.Model
-  # import Ecto.Changeset
   alias GraphBanking.Repo
   alias GraphBanking.Schema.Account
 
@@ -11,6 +9,7 @@ defmodule GraphBanking.Schema.Transfer do
     field :address, :binary_id
     field :amount, :float
     field :when, :utc_datetime_usec, default: DateTime.utc_now()
+
     belongs_to :account, Account,
       references: :uuid,
       foreign_key: :sender,
@@ -26,6 +25,25 @@ defmodule GraphBanking.Schema.Transfer do
     transfer
     |> cast(attrs, [:sender, :address, :amount])
     |> validate_required([:sender, :address, :amount])
+    |> validate_number(:sender, not_equal_to: :address)
     |> foreign_key_constraint(:sender)
+    |> sender_address_must_be_dif
+  end
+
+  def sender_address_must_be_dif(changeset) do
+    case changeset.valid? do
+      true ->
+        # IO.inspect changeset
+        sender = changeset.changes.sender
+        address = changeset.changes.address
+
+        case String.normalize(sender, :nfd) == String.normalize(address, :nfd) do
+          true -> add_error(changeset, :sender, "You cannot make a transfer to yourself")
+          _ -> changeset
+        end
+
+      _ ->
+        changeset
+    end
   end
 end
