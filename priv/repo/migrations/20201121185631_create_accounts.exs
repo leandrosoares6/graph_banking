52f6c609a0ae3ffb8f6035fbac993ab11bfb283e
Leandro Neves <leandrosoares6@hotmail.com>
2020-12-18 17:50:47 -0300

    Update migrations with string type instead of binary_id in UUIDS

--- priv/repo/migrations/20201121185631_create_accounts.exs
+++ priv/repo/migrations/20201121185631_create_accounts.exs
@@ -1,9 +1,10 @@
 defmodule GraphBanking.Repo.Migrations.CreateAccounts do
+  @moduledoc false
   use Ecto.Migration
 
   def change do
     create table(:accounts, primary_key: false) do
-      add :uuid, :binary_id, primary_key: true
+      add :uuid, :string, primary_key: true
       add :balance, :float
 
       timestamps()
--- priv/repo/migrations/20201123034820_create_transfers.exs
+++ priv/repo/migrations/20201123034820_create_transfers.exs
@@ -1,11 +1,12 @@
 defmodule GraphBanking.Repo.Migrations.CreateTransfers do
+  @moduledoc false
   use Ecto.Migration
 
   def change do
     create table(:transfers, primary_key: false) do
-      add :uuid, :binary_id, primary_key: true
-      add :sender, references(:accounts, column: :uuid, type: :binary_id)
-      add :address, :binary_id
+      add :uuid, :string, primary_key: true
+      add :sender, references(:accounts, column: :uuid, type: :string)
+      add :address, :string
       add :amount, :float
       add :when, :utc_datetime_usec
     end
