8a3d1632de2190b67d76220f9e0f7a6fc6696242
Leandro Neves <leandrosoares6@hotmail.com>
2020-12-20 08:13:59 -0300

    Business logic removed from transfer repository to use case

--- lib/graph_banking/domain/account/transfers_repository.ex
+++ lib/graph_banking/domain/account/transfers_repository.ex
@@ -5,24 +5,7 @@ defmodule GraphBanking.Repository.Transfers do
   alias GraphBanking.Repository.Accounts
   import Ecto.Changeset, only: [change: 2]
 
-  def create(transfer_model) do
-    sender = transfer_model.sender |> Accounts.get()
-    address = transfer_model.address |> Accounts.get()
-    transfer = transfer_model |> to_persistence_model()
-
-    case sender do
-      nil ->
-        {:error, "Sender not found."}
-
-      _ ->
-        case address do
-          nil -> {:error, "Address not found."}
-          _ -> transfer_money(sender, address, transfer)
-        end
-    end
-  end
-
-  defp transfer_money(sender, address, transfer) do
+  def create(sender, address, transfer) do
     amount = transfer.amount
     sender_balance = Accounts.debit(sender, amount)
     address_balance = Accounts.credit(address, amount)
@@ -48,16 +31,15 @@ defmodule GraphBanking.Repository.Transfers do
     end
   end
 
-  def create_model(args) do
-    insert = args |> Transfer.create()
+  def cast_to_model(args) do
+    args
+    |> Transfer.create()
+  end
 
-    case insert do
-      {:error, message} ->
-        {:error, message}
+  def cast_to_schema(transfer_model) do
+    transfer_model
+    |> to_persistence_model()
 
-      model_transfer ->
-        model_transfer
-    end
   end
 
   defp to_persistence_model(domain_model) do
--- lib/graph_banking/domain/account/use_cases/transfer_money.ex
+++ lib/graph_banking/domain/account/use_cases/transfer_money.ex
@@ -2,13 +2,36 @@ defmodule GraphBanking.Account.UseCases.TransferMoney do
   @moduledoc false
 
   alias GraphBanking.Repository.Transfers
+  alias GraphBanking.Repository.Accounts
 
   def apply(args) do
-    transfer_model = args |> Transfers.create_model()
+    transfer_model = args |> Transfers.cast_to_model()
 
     case transfer_model do
       {:error, error} -> {:error, error}
-      transfer -> transfer |> Transfers.create()
+      transfer -> transfer |> create()
+    end
+  end
+
+  defp create(transfer_model) do
+    sender = transfer_model.sender |> Accounts.get()
+    address = transfer_model.address |> Accounts.get()
+    transfer = transfer_model |> Transfers.cast_to_schema()
+
+    # cond do
+    #   sender == nil -> {:error, "Sender not found."}
+    #   address == nil -> {:error, "Address not found."}
+    # end
+
+    case sender do
+      nil ->
+        {:error, "Sender not found."}
+
+      _ ->
+        case address do
+          nil -> {:error, "Address not found."}
+          _ -> Transfers.create(sender, address, transfer)
+        end
     end
   end
 end
